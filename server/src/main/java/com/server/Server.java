22a0a654605d09728488e3e083ad0bdfeaadd10f
Markus Kelanti <markus.kelanti@oulu.fi>
2022-01-18 13:40:05 +0200

    Template created

---
+++ server/src/main/java/com/server/Server.java
@@ -0,0 +1,13 @@
+package com.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class Server
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Maven template for server" );
+    }
+}
